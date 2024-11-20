e5a64502c5b41d2678d734391c2068699ebd365a
Antinson <ant.mitchinson@gmail.com>
2024-11-20 15:51:07 +1300

    Finished day2

--- day2/day2_p2.py
+++ day2/day2_p2.py
@@ -1,10 +1,3 @@
-count_dict = {
-    'red' : 12,
-    'green' : 13,
-    'blue' : 14
-}
-
-
 with open('1.txt') as file:
     data = file.readlines()
 
@@ -12,29 +5,31 @@ res = 0
 
 for game in data:
     
-    valid_game = True
     game_id = int(game.split(':')[0][5:])
 
     game_all = game.split(':')[1].strip()
     game_indv = [x.lstrip(" ") for x in game_all.split(';')]
 
+    count_dict = {
+        'red': 0,
+        'green': 0,
+        'blue': 0
+    }
+
     for current_game in game_indv:
 
         current_game_split = [x.lstrip(" ") for x in current_game.split(',')]
         
-
         for value in current_game_split:
             num, colour = value.split(' ')
 
-            if count_dict[colour.lower()] < int(num):
-                valid_game = False
-                break
+            c = count_dict.get(colour, 0)
+            count_dict[colour] = c if int(num) < c else int(num)
+        
+
+    current_game_power = count_dict['red'] * count_dict['green'] * count_dict['blue']
+    print(f"game_id: {game_id} and result: {current_game_power}")
     
-    if valid_game:
-        res += game_id
-
+    res += current_game_power
+        
 print(res)
-            
-            
-
- 
